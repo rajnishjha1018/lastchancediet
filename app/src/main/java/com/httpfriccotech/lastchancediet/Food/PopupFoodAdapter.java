30e5a1ac609c5f0dae78b59c39fc153929be028e
Manish Singh <manish.singh@v3mobi.com>
2018-10-05 00:45:00 +0530

    food popup changes layout

--- app/src/main/java/com/httpfriccotech/lastchancediet/Food/PopupFoodAdapter.java
+++ app/src/main/java/com/httpfriccotech/lastchancediet/Food/PopupFoodAdapter.java
@@ -3,13 +3,11 @@ package com.httpfriccotech.lastchancediet.Food;
 import android.content.Context;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.PopupWindow;
-import android.widget.Toast;
 
 import com.httpfriccotech.lastchancediet.R;
 
@@ -59,7 +57,7 @@ public class PopupFoodAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
         viewHolder.txtFiber.setText(myData.getFiber());
         viewHolder.textAddFood.setTag(myData.getTitle());
         viewHolder.textAddFood.setVisibility(myData.getProtein().equalsIgnoreCase("0") ? View.VISIBLE : View.INVISIBLE);
-        viewHolder.titleTV.setVisibility(View.GONE);
+        viewHolder.titleTV.setText(myData.getTitle());
         viewHolder.container.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
