239f47ee66b3d0517d987e45e273a5aee01b92b4
rajnish Kumar <rajnish@eliteanswers.com>
2018-11-02 17:39:50 +0530

    remove conflict

--- app/src/main/java/com/httpfriccotech/lastchancediet/network/CommunicationConstants.java
+++ app/src/main/java/com/httpfriccotech/lastchancediet/network/CommunicationConstants.java
@@ -7,10 +7,9 @@ package com.httpfriccotech.lastchancediet.network;
 public class CommunicationConstants {
     /*public static final String BASE_URL_KNOWLAGE = "https://laststaging.wpengine.com/wp-json/users/v1/";*/
 //    public static final String BASE_URL_KNOWLAGE = "https://laststaging.wpengine.com/wp-json/users/v1/";
-   // public static final String BASE_URL_KNOWLAGE = "https://laststaging.wpengine.com/wp-json/";
-
-  public static final String BASE_URL_KNOWLAGE = "https://lastchancediet.wpengine.com/wp-json/";
-
-   // public static final String BASE_URL_KNOWLAGE = "https://lastchancediet.wpengine.com/wp-json/";
-
+//    public static final String BASE_URL_KNOWLAGE = "https://laststaging.wpengine.com/wp-json/wp/v2/";
+    public static final String BASE_URL_KNOWLAGE = "https://laststaging.wpengine.com/wp-json/";//dev
+    //    public static final String BASE_URL_KNOWLAGE = "https://lastchancediet.wpengine.com/wp-json/";//prod
+//    public static final String BASE_URL_AUTHENTICATION = "https://laststaging.wpengine.com/wp-json/";
+    public static final String SIGNUP_RL="https://laststaging.wpengine.com/signup/";
 }
