f7450d2e9018deafb2dc8b3da6ebb9e4bff0914d
Manish Singh <manish.singh@v3mobi.com>
2018-11-28 00:06:35 +0530

    Forgot password changes done

--- app/src/main/java/com/httpfriccotech/lastchancediet/network/CommunicationConstants.java
+++ app/src/main/java/com/httpfriccotech/lastchancediet/network/CommunicationConstants.java
@@ -9,7 +9,7 @@ public class CommunicationConstants {
     //    public static final String BASE_URL_KNOWLAGE = "https://laststaging.wpengine.com/wp-json/users/v1/";
     //    public static final String BASE_URL_KNOWLAGE = "https://laststaging.wpengine.com/wp-json/wp/v2/";
    // public static final String BASE_URL_KNOWLAGE = "https://laststaging.wpengine.com/wp-json/";//dev
-    public static final String BASE_URL_KNOWLAGE = "https://www.lastchancediet.com/wp-json/";//prod
+    public static final String BASE_URL_KNOWLAGE = "https://www.lastchancediet.com/";//prod
     //public static final String BASE_URL_AUTHENTICATION = "https://laststaging.wpengine.com/wp-json/";
     //  public static final String SIGNUP_RL="https://laststaging.wpengine.com/signup/";
     public static final String SIGNUP_RL="https://www.lastchancediet.com/signup?mtype=true";
